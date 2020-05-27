961c8a1725deb891d57b1f77544c1344bafd293a
Алексей <43031739+tsyploff@users.noreply.github.com>
2020-05-27 12:34:54 +0300

    Add files via upload

--- test_2/index.php
+++ test_2/index.php
@@ -2,10 +2,10 @@
 <!DOCTYPE html>
 <html lang="ru">
 	<head>
-		<meta charset="utf-8"> 										<!-- кодировка -->
-		<title>Цыплов Алексей Михайлович, Вариант 11</title> 		<!-- имя вкладки -->
-		<link rel="stylesheet" type="text/css" href="css/main.css"> <!-- подключаем css -->
-		<script type="text/javascript", src="js/main.js"></script> 	<!-- подключаем js -->
+		<meta charset="utf-8">										<!-- кодировка -->
+		<title>Цыплов Алексей Михайлович, Вариант 11</title>		<!-- имя вкладки -->
+		<link rel="stylesheet" type="text/css" href="css/main.css">	<!-- подключаем css -->
+		<script type="text/javascript", src="js/main.js"></script>	<!-- подключаем js -->
 	</head>
 	<body>
 		<h1>Алексей Цыплов</h1>		<!-- имя владельца -->
