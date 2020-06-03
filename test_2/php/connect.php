1044418e0f51bee0ef88c082de7d0d7a67e50ce9
Алексей <43031739+tsyploff@users.noreply.github.com>
2020-06-03 10:11:47 +0300

    Delete connect.php

--- test_2/php/connect.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-	$db = mysqli_connect("localhost", "u11", "magenta_july", "u11_test_2"); //подключение базы данных
-
-	if (!$db) //обработка ошибки подключения
-		echo "error!" . mysqli_connect_error();
-	
-	mysqli_set_charset($db, 'utf8'); //установка кодировки
